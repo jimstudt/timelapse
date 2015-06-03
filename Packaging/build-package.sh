de41f626536eb7a3a0f100ad82cfb954e9377e10
Jim Studt <jim@studt.net>
2015-06-03 14:23:59 -0500

    A little cleanup for shell expansion to tolerate spaces in path components

--- Packaging/build-package.sh
+++ Packaging/build-package.sh
@@ -7,6 +7,7 @@
 #  Copyright (c) 2014 Lunarware. All rights reserved.
 
 set -e
+set -x
 
 if ( echo "${BUILT_PRODUCTS_DIR}" | fgrep "/Debug/" ) ;then
     echo You are trying to build a package of a Debug release. Not what you want.
@@ -15,24 +16,24 @@ fi
 
 CERTIFICATE_CN="Developer ID Installer: James Studt (HJS98U3F75)"
 
-VERSION=$(sed -E -n -e 's/#define *TIMELAPSE_VERSION *"([^"]*)"/\1/p' ${SOURCE_ROOT}/Version.h)
+VERSION=$(sed -E -n -e 's/#define *TIMELAPSE_VERSION *"([^"]*)"/\1/p' "${SOURCE_ROOT}"/Version.h)
 PACKAGE_NAME=`echo "$PRODUCT_NAME" | sed "s/ /_/g"`
 
 FAKEROOT=${BUILT_PRODUCTS_DIR}/install-usr
 COMPONENT_PACKAGE=${BUILT_PRODUCTS_DIR}/timelapse.pkg
 
-rm -rf ${FAKEROOT}
-mkdir ${FAKEROOT}
-mkdir -p ${FAKEROOT}/usr/local/bin
-mkdir -p ${FAKEROOT}/usr/local/share/man/man1
-cp ${BUILT_PRODUCTS_DIR}/timelapse ${FAKEROOT}/usr/local/bin/
-cp ${SOURCE_ROOT}/timelapse.1 ${FAKEROOT}/usr/local/share/man/man1/
+rm -rf "${FAKEROOT}"
+mkdir "${FAKEROOT}"
+mkdir -p "${FAKEROOT}/usr/local/bin"
+mkdir -p "${FAKEROOT}/usr/local/share/man/man1"
+cp "${BUILT_PRODUCTS_DIR}/timelapse" "${FAKEROOT}/usr/local/bin/"
+cp "${SOURCE_ROOT}/timelapse.1" "${FAKEROOT}/usr/local/share/man/man1/"
 
-pkgbuild --root ${FAKEROOT} \
+pkgbuild --root "${FAKEROOT}" \
     --identifier com.lunarware.timelapse \
     --version "${VERSION}" \
     --ownership recommended \
     --sign "${CERTIFICATE_CN}" \
-    ${COMPONENT_PACKAGE}
+    "${COMPONENT_PACKAGE}"
 
 
